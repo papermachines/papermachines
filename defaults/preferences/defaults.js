a72bc6fc483d7224a40a6ff407a59b663bc3d7b0
Chris Johnson-Roberson <chris.johnson.roberson@gmail.com>
2013-03-07 01:32:48 -0500

    stopwords pane

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -29,10 +29,12 @@ pref("extensions.papermachines.lda.alpha", "50.0");
 pref("extensions.papermachines.lda.beta", "0.01");
 pref("extensions.papermachines.lda.burn_in", 200);
 
-pref("extensions.papermachines.lda.optimize_interval", 0);
+pref("extensions.papermachines.lda.optimize_interval", 10);
 pref("extensions.papermachines.lda.symmetric_alpha", false);
 
 pref("extensions.papermachines.lda.stemming", true);
 pref("extensions.papermachines.lda.segmentation", false);
 pref("extensions.papermachines.lda.tfidf", true);
 pref("extensions.papermachines.lda.min_df", 3);
+
+pref("extensions.papermachines.stopwords", "");
