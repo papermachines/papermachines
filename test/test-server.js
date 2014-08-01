6b942c6c1a00281d82d6ac796c18795097bcd518
Chris Johnson-Roberson <chris.johnson.roberson@gmail.com>
2014-08-01 14:27:55 -0400

    clean up server test

--- test/test-server.js
+++ test/test-server.js
@@ -35,8 +35,7 @@ exports["test create new corpus"] = function(assert, done) {
     var corpusID = server.findOrCreateCorpus("Testing", "-1");
     corpusID.then(function(id) {
         assert.ok(id, "Corpus created");
-        done();
-    }, onError(assert, done));
+    }, onError(assert, done)).then(done, onError(assert, done));
 };
 
 exports["test create corpus exactly once"] = function(assert, done) {
@@ -46,8 +45,7 @@ exports["test create corpus exactly once"] = function(assert, done) {
     server.findOrCreateCorpus(name, externalID).then(function(id1) {
         return server.findOrCreateCorpus(name, externalID).then(function(id2) {
             assert.equal(id1, id2, "Corpus is only created once");
-            done();
-        }, onError(assert, done));
+        }, onError(assert, done)).then(done, onError(assert, done));
     }, onError(assert, done));
 };
 
@@ -58,8 +56,7 @@ exports["test add item to corpus"] = function(assert, done) {
         assert.ok(result, "Item created");
         assert.equal(result.statusText, "Created", "Item was created");
         assert.equal(result.message, "new", "Item is new");
-        done();
-    }, onError(assert, done)).then(null, onError(assert, done));
+    }, onError(assert, done)).then(done, onError(assert, done));
 };
 
 exports["test add item only once"] = function(assert, done) {
@@ -69,12 +66,11 @@ exports["test add item only once"] = function(assert, done) {
         assert.ok(result, "Item created");
         assert.equal(result.statusText, "Created", "Item was created");
     }, onError(assert, done)).then(function() {
-        return server.sendItemToServer(corpusID, fakeItemOnlyOnce)
+        return server.sendItemToServer(corpusID, fakeItemOnlyOnce);
     }, onError(assert, done)).then(function(result) {
         assert.equal(result.statusText, "OK", "Item was found");
         assert.equal(result.message, "found", "Item already existed");
-        done();
-    }, onError(assert, done)).then(null, onError(assert, done));
+    }, onError(assert, done)).then(done, onError(assert, done));
 };
 
 exports["test update item if newer"] = function(assert, done) {
@@ -87,8 +83,7 @@ exports["test update item if newer"] = function(assert, done) {
     }, onError(assert, done)).then(function(result) {
         assert.equal(result.statusText, "OK", "Item existed");
         assert.equal(result.message, "updated", "Item was altered");
-        done();
-    }, onError(assert, done)).then(null, onError(assert, done));
+    }, onError(assert, done)).then(done, onError(assert, done));
 };
 
 before(exports, function() {
